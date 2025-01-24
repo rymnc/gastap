3930062f78da58498cb6ea2d11c48a893c6f59fd
Aaryamann Challani <43716372+rymnc@users.noreply.github.com>
2025-01-24 23:21:08 +0530

    fix

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,10 @@
 use fuel_core::combined_database::CombinedDatabase;
-use fuel_core::state::historical_rocksdb::StateRewindPolicy;
 use fuel_core::state::rocks_db::DatabaseConfig;
 use std::path::Path;
 
 pub fn get_db(path: &Path) -> anyhow::Result<CombinedDatabase> {
-    CombinedDatabase::open(
+    CombinedDatabase::open_read_only(
         path,
-        StateRewindPolicy::NoRewind,
         DatabaseConfig {
             cache_capacity: None,
             max_fds: -1,
